6f42bd4c048105b86af108761cfa0f0f6875d6a7
jm4R <mar.jaskolka@gmail.com>
2019-05-21 17:47:52 +0200

    Refactoring – apply hungarian notation for members

--- src/mapp.hpp
+++ src/mapp.hpp
@@ -46,55 +46,55 @@ class audio {
 public:
     void wait() const
     {
-        if (!silence) {
-            std::unique_lock<std::mutex> lock{ mutex };
-            cv_finished.wait(lock, [this] { return silence; });
+        if (!m_silence) {
+            std::unique_lock<std::mutex> lock{ m_mutex };
+            m_cv_finished.wait(lock, [this] { return m_silence; });
         }
     }
 
     bool is_playing() const
     {
-        return !silence;
+        return !m_silence;
     }
 
     /// Stops device asynchronously. Call wait to ensure audio is stopped. Finish callback will be called.
     void stop()
     {
-        stop_later = true;
+        m_stop_later = true;
     }
 
     /// Callback can not call mapp API, see https://github.com/dr-soft/miniaudio/issues/64
     void set_finish_callback(std::function<void()> callback)
     {
-        on_finish_callback = std::move(callback);
+        m_on_finish_callback = std::move(callback);
     }
 
 protected:
     audio() /// This class is not supposed to be used directly.
-        : decoder{}
+        : m_decoder{}
     {
     }
 
 private:
     void rewind()
     {
-        stop_later = false;
-        ma_decoder_seek_to_pcm_frame(&decoder, 0);
+        m_stop_later = false;
+        ma_decoder_seek_to_pcm_frame(&m_decoder, 0);
     }
 
     unsigned data(void* output, unsigned frame_count)
     {
-        const auto framesDecoded = ma_decoder_read_pcm_frames(&decoder, output, frame_count);
+        const auto framesDecoded = ma_decoder_read_pcm_frames(&m_decoder, output, frame_count);
 
-        bool silenceNow = framesDecoded == 0 || stop_later;
-        if (silenceNow && !silence) {
+        bool silenceNow = framesDecoded == 0 || m_stop_later;
+        if (silenceNow && !m_silence) {
             {
-                std::lock_guard<std::mutex> lock { mutex };
-                silence = true;
+                std::lock_guard<std::mutex> lock { m_mutex };
+                m_silence = true;
             }
             finish_playing_callback();
         } else {
-            silence = silenceNow;
+            m_silence = silenceNow;
         }
 
         return unsigned(framesDecoded);
@@ -102,20 +102,20 @@ private:
 
     void finish_playing_callback()
     {
-        if (on_finish_callback)
-            on_finish_callback();
-        cv_finished.notify_all();
+        if (m_on_finish_callback)
+            m_on_finish_callback();
+        m_cv_finished.notify_all();
     }
 
 protected:
-    ma_decoder decoder;
+    ma_decoder m_decoder;
 
 private:
-    mutable std::mutex mutex{};
-    mutable std::condition_variable cv_finished{};
-    std::function<void()> on_finish_callback{};
-    bool silence{ true };
-    bool stop_later{ false };
+    mutable std::mutex m_mutex{};
+    mutable std::condition_variable m_cv_finished{};
+    std::function<void()> m_on_finish_callback{};
+    bool m_silence{ true };
+    bool m_stop_later{ false };
 };
 
 /// Audio played directly from the file
@@ -125,7 +125,7 @@ public:
     audio_file(const char* file_name)
     {
         ma_decoder_config cfg = ma_decoder_config_init(ma_format_f32, 2, 44100); //TODO read from device
-        ma_result result = ma_decoder_init_file(file_name, &cfg, &decoder);
+        ma_result result = ma_decoder_init_file(file_name, &cfg, &m_decoder);
         if (result != MA_SUCCESS) {
             throw mapp_exception{};
         }
@@ -138,7 +138,7 @@ public:
     audio_memory_view(const void* data, std::size_t size)
     {
         ma_decoder_config cfg = ma_decoder_config_init(ma_format_f32, 2, 44100); //TODO read from device
-        ma_result result = ma_decoder_init_memory(data, size, &cfg, &decoder);
+        ma_result result = ma_decoder_init_memory(data, size, &cfg, &m_decoder);
         if (result != MA_SUCCESS) {
             throw mapp_exception{};
         }
@@ -157,17 +157,17 @@ class oastream final {
 
 public:
     explicit oastream(const oastream_config& config = oastream_config{})
-        : device{}
-        , dev_config(make_ma_config(config))
+        : m_device{}
+        , m_dev_config(make_ma_config(config))
     {
-        if (ma_device_init(nullptr, &dev_config, &device) != MA_SUCCESS) {
+        if (ma_device_init(nullptr, &m_dev_config, &m_device) != MA_SUCCESS) {
             throw mapp_exception{};
         }
     }
 
     ~oastream()
     {
-        ma_device_uninit(&device);
+        ma_device_uninit(&m_device);
     }
 
     oastream(const oastream&) = delete;
@@ -186,37 +186,37 @@ public:
     /// Audios finish callbacks will not be invoked.
     void stop_audios()
     {
-        stop_later = true;
+        m_stop_later = true;
     }
 
     /// Removes all audios and stops stream. Call wait before destroing any audio.
     void stop_stream()
     {
         stop_audios();
-        ma_device_stop(&device);
+        ma_device_stop(&m_device);
     }
 
     /// Starts when the stream is stopped.
     void play(audio& audio)
     {
         audio.rewind();
-        audio.silence = false;
-        audios.push_back(&audio);
+        audio.m_silence = false;
+        m_audios.push_back(&audio);
         play_impl();
     }
 
     void wait() const
     {
-        if (!silence) {
-            std::unique_lock<std::mutex> lock{ mutex };
-            cv_finished.wait(lock, [this] { return silence; });
+        if (!m_silence) {
+            std::unique_lock<std::mutex> lock{ m_mutex };
+            m_cv_finished.wait(lock, [this] { return m_silence; });
         }
     }
 
     /// Sets a volume in (0.0 - 1.0) range. Setting volume higher than 1.0 may cause crackles.
     void set_volume(float value)
     {
-        volume = value;
+        m_volume = value;
     }
 
 private:
@@ -229,37 +229,37 @@ private:
 
     void data_callback_impl(void* pOutput, ma_uint32 frameCount)
     {
-        std::size_t buffer_size_frames = frameCount * dev_config.playback.channels;
+        std::size_t buffer_size_frames = frameCount * m_dev_config.playback.channels;
 
-        frames_buffer.reserve(buffer_size_frames);
-        float32* audio_output = frames_buffer.data();
+        m_frames_buffer.reserve(buffer_size_frames);
+        float32* audio_output = m_frames_buffer.data();
         auto fOutput = static_cast<float32*>(pOutput);
         std::memset(fOutput, 0, buffer_size_frames * sizeof(float32));
 
-        for (auto* audio : audios) {
+        for (auto* audio : m_audios) {
             const auto framesDecoded = audio->data(audio_output, frameCount);
-            for (unsigned i = 0; i < framesDecoded * dev_config.playback.channels; ++i)
-                fOutput[i] += volume * audio_output[i];
+            for (unsigned i = 0; i < framesDecoded * m_dev_config.playback.channels; ++i)
+                fOutput[i] += m_volume * audio_output[i];
         }
 
         // Remove all finished audios:
-        auto toRemove = stop_later ? audios.begin()
-                                    : std::remove_if(audios.begin(), audios.end(), [](const audio* a) { return !a->is_playing(); });
-        audios.erase(toRemove, audios.end());
-        if (audios.empty() && !silence) {
+        auto toRemove = m_stop_later ? m_audios.begin()
+                                    : std::remove_if(m_audios.begin(), m_audios.end(), [](const audio* a) { return !a->is_playing(); });
+        m_audios.erase(toRemove, m_audios.end());
+        if (m_audios.empty() && !m_silence) {
             {
-                std::lock_guard<std::mutex> lock{ mutex };
-                silence = true;
+                std::lock_guard<std::mutex> lock{ m_mutex };
+                m_silence = true;
             }
             finish_playing_callback();
         }
-        silence = audios.empty();
+        m_silence = m_audios.empty();
     }
 
     void finish_playing_callback()
     {
-        stop_later = false;
-        cv_finished.notify_all();
+        m_stop_later = false;
+        m_cv_finished.notify_all();
     }
 
     ma_device_config make_ma_config(const oastream_config& oa_cfg)
@@ -276,24 +276,24 @@ private:
 
     void play_impl()
     {
-        silence = false;
-        if (ma_device__get_state(&device) != MA_STATE_STOPPED) //TODO: race condition
+        m_silence = false;
+        if (ma_device__get_state(&m_device) != MA_STATE_STOPPED) //TODO: race condition
             return;
-        if (ma_device_start(&device) != MA_SUCCESS) {
+        if (ma_device_start(&m_device) != MA_SUCCESS) {
             throw mapp_exception{};
         }
     }
 
 private:
-    ma_device device;
-    ma_device_config dev_config;
-    mutable std::mutex mutex;
-    mutable std::condition_variable cv_finished;
-    std::vector<audio*> audios;
-    std::vector<float32> frames_buffer;
-    float volume { 1.0f };
-    bool stop_later{ false };
-    bool silence{ true };
+    ma_device m_device;
+    ma_device_config m_dev_config;
+    mutable std::mutex m_mutex;
+    mutable std::condition_variable m_cv_finished;
+    std::vector<audio*> m_audios;
+    std::vector<float32> m_frames_buffer;
+    float m_volume { 1.0f };
+    bool m_stop_later{ false };
+    bool m_silence{ true };
 };
 
 oastream& operator<<(oastream& aout, audio& a)
